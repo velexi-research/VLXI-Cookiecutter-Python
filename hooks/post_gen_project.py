3aff5cec065b1d8ce19a0ca6134c788a671dfe0a
Kevin Chu <kevin@velexi.com>
2022-10-07 20:03:56 -0700

    Add automation steps to cookiecutter hooks.

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -13,10 +13,13 @@ import subprocess
 
 _PROJECT_DIRECTORY = Path.cwd()
 
-
 # --- Utility functions
 
+
 def _remove_file(*filepath):
+    """
+    Remove file.
+    """
     try:
         Path(_PROJECT_DIRECTORY, *filepath).unlink()
     except FileNotFoundError:
@@ -32,8 +35,33 @@ if __name__ == "__main__":
     if "{{ cookiecutter.license }}" != "Apache License 2.0":
         _remove_file("NOTICE")
 
-    # --- Initialize Git repository project
+    # --- Set up Git repository for project
 
     os.chdir(_PROJECT_DIRECTORY)
+
+    # Initialize Git repository
     cmd = ["git", "init"]
+    subprocess.run(cmd, check=True)
+
+    # Commit cookiecutter files
+    cmd = ["git", "add", "."]
+    subprocess.run(cmd, check=True)
+
+    cmd = ["git", "commit", "-m", "Initial commit."]
+    subprocess.run(cmd, check=True)
+
+    # Create branch for deployment of package documentation to GitHub pages
+    if "{{ cookiecutter.enable_github_pages }}" == "yes":
+
+        # Create gh-pages branch
+        cmd = ["git", "checkout", "-b", "gh-pages"]
+        subprocess.run(cmd, check=True)
+
+        # Change back to "main" branch
+        cmd = ["git", "checkout", "main"]
+        subprocess.run(cmd, check=True)
+
+    # --- Install Git pre-commit hooks.
+
+    cmd = ["pre-commit", "install"]
     subprocess.run(cmd, check=True)
